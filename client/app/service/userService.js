fd64743eb2b8f91848df07f402cdaf3ceba7d01c
math <master_of_puppets57@hotmail.fr>
2017-10-23 11:30:16 +0200

    création du répertoire de services

---
+++ client/app/service/userService.js
@@ -0,0 +1,9 @@
+app.factory("userFactory", ['$resource', function($resource) {
+    return $resource('/api/liste/userId', {
+        userId: '@_id'}, {
+            update: {
+                method: 'PUT'
+            }
+        });
+    }
+]);
